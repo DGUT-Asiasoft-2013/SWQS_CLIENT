bbd1b7539d2fa9b9d51ed669ffaecc08006ed7e6
KennyYuan-7 <382242303@qq.com>
2016-12-21 17:51:18 +0800

    初步实现发布商品页面

---
+++ src/com/swqs/schooltrade/NewGoodsActivity.java
@@ -0,0 +1,110 @@
+package com.swqs.schooltrade;
+
+import java.io.IOException;
+
+import com.swqs.schooltrade.api.Server;
+import com.swqs.schooltrade.fragments.inputcells.ImageInputCellFragment;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class NewGoodsActivity extends Activity {
+    EditText editTitle, editContent, editOriginalPrice, editCurPrice;
+    ImageInputCellFragment fragInputImage;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.activity_new_goods);
+    	
+    	editTitle = (EditText) findViewById(R.id.edit_title);
+    	editContent = (EditText) findViewById(R.id.edit_content);
+    	editOriginalPrice = (EditText) findViewById(R.id.edit_originalprice);
+    	editCurPrice = (EditText) findViewById(R.id.edit_curprice);
+    	fragInputImage = (ImageInputCellFragment) getFragmentManager().findFragmentById(R.id.input_image);
+    	
+    	findViewById(R.id.button_publish).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				publishContent();
+			}
+		});
+    }
+    
+    void publishContent(){
+    	String title = editTitle.getText().toString();
+    	String content = editContent.getText().toString();
+    	String originalprice = editOriginalPrice.getText().toString();    	
+    	String curprice = editCurPrice.getText().toString();
+    	
+    	OkHttpClient client = Server.getSharedClient();
+    	
+    	MultipartBody.Builder body = new MultipartBody.Builder()
+				.addFormDataPart("title", title)
+				.addFormDataPart("content", content)
+				.addFormDataPart("originalprice", originalprice)
+				.addFormDataPart("curprice", curprice);				
+    	
+    	if(fragInputImage.getPngData()!=null){
+			body.addFormDataPart(
+					"image",
+					"image",
+					RequestBody.create(MediaType.parse("image/png"),
+					fragInputImage.getPngData()));
+		}
+    	
+    	
+    	Request request = Server.requestBuilderWithApi("addgoods").post(body.build()).build();
+
+		Server.getSharedClient().newCall(request).enqueue(new Callback() {
+			@Override
+			public void onResponse(Call arg0, Response arg1) throws IOException {
+				final String responseBody = arg1.body().string();
+
+				runOnUiThread(new Runnable() {
+					public void run() {
+						NewGoodsActivity.this.onSucceed(responseBody);
+					}
+				});
+			}
+
+			@Override
+			public void onFailure(Call arg0, final IOException arg1) {
+				runOnUiThread(new Runnable() {
+					public void run() {
+						NewGoodsActivity.this.onFailure(arg1);
+					}
+				});
+			}
+		});
+	}
+
+	void onSucceed(String text){
+		new AlertDialog.Builder(this).setMessage(text)
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+				overridePendingTransition(R.anim.none, R.anim.slide_out_bottom);
+			}
+		}).show();
+	}
+
+	void onFailure(Exception e){
+		new AlertDialog.Builder(this).setMessage(e.getMessage()).show();
+	}
+}
